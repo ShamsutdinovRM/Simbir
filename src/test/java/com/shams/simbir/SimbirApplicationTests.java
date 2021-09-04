ccb6ad75d98e67a69b509c0e5f83cf7e7b6697d4
Ruslan <protego081200@gmail.com>
2021-09-04 22:26:00 +0500

    Initial commit

---
+++ src/test/java/com/shams/simbir/SimbirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shams.simbir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimbirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
